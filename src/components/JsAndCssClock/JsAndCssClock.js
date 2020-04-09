1a23984377708cd8f66e50dc4d16306cdf450d67
Simon Nguyen <simonnguyen3054@gmail.com>
2020-04-08 23:26:22 -0700

    added clock to the wall

--- src/components/JsAndCssClock/JsAndCssClock.js
+++ src/components/JsAndCssClock/JsAndCssClock.js
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.scss";
 
 const JsAndCssClock = () => {
+  const [secondDegrees, setSecondDegrees] = useState(90);
+  const [minuteDegrees, setMinuteDegrees] = useState(90);
+  const [hourDegrees, setHourDegrees] = useState(90);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTime();
+    }, 1000);
+  }, [secondDegrees]);
+
+  const setTime = () => {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondDegreesConverter = (seconds / 60) * 360 + 90;
+    setSecondDegrees(secondDegreesConverter);
+
+    const minutes = now.getMinutes();
+    const minuteDegreesConverter = (minutes / 60) * 360 + 90;
+    setMinuteDegrees(minuteDegreesConverter);
+
+    const hours = now.getHours();
+    const hourDegreesConverter = (hours / 12) * 360 + 90;
+    setHourDegrees(hourDegreesConverter);
+  };
+
   return (
     <div className="clockBackground">
       <div className="clock">
         <div className="clock-inner">
           <div className="clock-face">
-            <div className="hand hour-hand"></div>
-            <div className="hand min-hand"></div>
-            <div className="hand second-hand"></div>
+            <div
+              style={{ transform: `rotate(${hourDegrees}deg)` }}
+              className="hand hour-hand"
+            ></div>
+            <div
+              style={{ transform: `rotate(${minuteDegrees}deg)` }}
+              className="hand min-hand"
+            ></div>
+            <div
+              style={{ transform: `rotate(${secondDegrees}deg)` }}
+              className="hand sec-hand"
+            ></div>
           </div>
         </div>
       </div>
